ef1a770191988efa262f767f08c5fe0a5e2067ad
R41NB0WD4SH <116415348+R41NB0WD4SH@users.noreply.github.com>
2023-04-14 13:15:55 +0300

    1

--- Hillel_Lesson5_HW/SquareRootsCalculator.cs
+++ Hillel_Lesson5_HW/SquareRootsCalculator.cs
@@ -39,21 +39,16 @@ public static class SquareRootsCalculator
 
     }
 
-    // public static double[] GetIntegerRoots(int[] numbers)
-    // {
-    //     double[] finalRoots = new double[0];
-    //
-    //     for (int i = 0; i < numbers.Length; i++)
-    //     {
-    //         if (numbers[i].GetRoot() > 5 || (numbers[i].GetRoot() % 1 == 0))
-    //         {
-    //             Array.Resize(ref finalRoots, finalRoots.Length + 1);
-    //             finalRoots[finalRoots.Length - 1] = (numbers[i].GetRoot());
-    //         }
-    //     }
-    //
-    //     return finalRoots;
-    // }
+    static Func<int, bool> numbersFilter = a => a > 5;
+
+    public static void GetNumbers(Func<int, bool> filter)
+    {
+        for (int i = 0; i < 100; i++)
+            if (filter(i))
+                Console.WriteLine(i);
+    }
+
+    
     
 }
 
